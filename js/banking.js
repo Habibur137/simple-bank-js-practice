0dd9bfaa7feca9eeeb0fe958901b7b2cea6390a2
Habibur137 <infernohabib@gmail.com>
2022-02-12 12:02:13 +0600

    get input function for deposit and withdraw

---
+++ js/banking.js
@@ -0,0 +1,24 @@
+document.getElementById('deposit-button').addEventListener('click', function(){
+    //take input value from input field
+    const depositAmount = getInput('deposit-input')
+    // set amount to the deposit container
+    setInput(depositAmount, 'deposit-container')
+    /* const depositContainer = document.getElementById('deposit-container')
+    const previousDepositAmount = parseFloat(depositContainer.innerText);
+    const totalAmount = previousDepositAmount + depositAmount;
+    depositContainer.innerText = totalAmount; */
+})
+document.getElementById('withdraw-button').addEventListener('click', function(){
+    //take input value from input field
+    const withdrawAmount = getInput('withdraw-input')
+    // set amount to the deposit container
+    setInput(withdrawAmount, 'withdraw-container')
+})
+
+//get input function for deposit and withdraw
+function getInput(input){
+    const getInputElement = document.getElementById(input);
+    const getInputValue = parseFloat(getInputElement.value);
+    getInputElement.value = '';
+    return getInputValue;
+}
